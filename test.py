9581440fa4a7eb0c445d63375745b1456cca48e7
nukunuku7 <masato_kumano@icloud.com>
2025-12-10 19:11:16 +0900

    振出しに戻る。

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import os
-import ctypes
-
-opencv_bin = r"C:\Users\vrlab\.vscode\nukunuku\OpenCV_CUDAbuild\opencv\x64\vc17\bin"
-os.environ['PATH'] = opencv_bin + ";" + os.environ['PATH']
-
-# OpenCV DLL のロード確認
-opencv_dll = os.path.join(opencv_bin, "opencv_world4130.dll")
-ctypes.WinDLL(opencv_dll)
-
-# cv2 のインポート
+import sys
+sys.path.pop(0)  # カレントディレクトリを import path から除去
 import cv2
+
 print(cv2.getBuildInformation())

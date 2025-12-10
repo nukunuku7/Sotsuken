8c642f75c3a3d21bf7c9547105119f56011fefe6
nukunuku7 <masato_kumano@icloud.com>
2025-12-10 10:24:36 +0900

    いったんクリーンな状態でGPU化中のやつをコミットします。

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-import cupy
-print("CuPy version:", cupy.__version__)
-cupy.show_config()
+import os
+import ctypes
 
-import cupy.cuda.runtime as rt
-try:
-    print("Runtime version:", rt.runtimeGetVersion())
-except Exception as e:
-    print("runtimeGetVersion ERROR:", e)
+opencv_bin = r"C:\Users\vrlab\.vscode\nukunuku\OpenCV_CUDAbuild\opencv\x64\vc17\bin"
+os.environ['PATH'] = opencv_bin + ";" + os.environ['PATH']
+
+# OpenCV DLL のロード確認
+opencv_dll = os.path.join(opencv_bin, "opencv_world4130.dll")
+ctypes.WinDLL(opencv_dll)
+
+# cv2 のインポート
+import cv2
+print(cv2.getBuildInformation())
